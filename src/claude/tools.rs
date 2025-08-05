85416d6e8edffef2ba892f7d102e22cf02ead046
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-08-05 11:47:46 -0700

    use consts for tool definitions

--- src/claude/tools.rs
+++ src/claude/tools.rs
@@ -1,50 +1,58 @@
 use serde::Serialize;
 use serde_json::json;
 
+pub mod literals {
+    pub const SEND_MESSAGE_NAME: &str = "send_message";
+    pub const SEND_MESSAGE_CONTENT_ARGUMENT_NAME: &str = "message_content";
+    pub const REACT_TO_MESSAGE_NAME: &str = "react_to_message";
+    pub const REACT_TO_MESSAGE_EMOJI_ARGUMENT_NAME: &str = "emoji";
+    pub const SKIP_RESPONSE_NAME: &str = "skip_response";
+}
+
 #[derive(Clone, Debug, Serialize)]
-pub struct Tool {
+pub struct ToolDefinition {
     pub name: String,
     pub description: String,
     pub input_schema: serde_json::Value,
 }
 
-impl Tool {
-    pub fn get_tools() -> Vec<Tool> {
+impl ToolDefinition {
+    pub fn get_tools() -> Vec<ToolDefinition> {
         vec![
-            Tool {
-                name: String::from("send_message"),
+            ToolDefinition {
+                name: String::from(literals::SEND_MESSAGE_NAME),
                 description: String::from(
                     "Sends a message in the current Discord text channel. Use this tool when you want to send a message. The `message_content` defines the text that will be included in the message.",
                 ),
                 input_schema: json!({
                   "type": "object",
                   "properties": {
-                    "message_content": {
+                    literals::SEND_MESSAGE_CONTENT_ARGUMENT_NAME: {
                       "type": "string",
                       "description": "The text to use for the Discord message body",
                     },
                   },
-                  "required": ["message_content"],
+                  "required": [literals::SEND_MESSAGE_CONTENT_ARGUMENT_NAME],
                 }),
             },
-            Tool {
-                name: String::from("react_to_message"),
+            ToolDefinition {
+                name: String::from(literals::REACT_TO_MESSAGE_NAME),
                 description: String::from(
                     "React to the most recent message with an emoji. Use this tool when you want to react to a Discord message. The `emoji` parameter define what emoji to use for the reaction. The `emoji` parameter should contain a single, valid, emoji like '😅' or '😅'",
                 ),
                 input_schema: json!({
                   "type": "object",
                   "properties": {
-                    "emoji": {
+                    literals::REACT_TO_MESSAGE_EMOJI_ARGUMENT_NAME: {
                       "type": "string",
                       "description": "Emoji to react with (e.g., '❤️', '👍', '🤔')",
                     },
                   },
-                  "required": ["emoji"],
+                  "required": [literals::REACT_TO_MESSAGE_EMOJI_ARGUMENT_NAME],
                 }),
             },
-            Tool {
-                name: String::from("skip_response"),
+            ToolDefinition {
+                name: String::from(literals::SKIP_RESPONSE_NAME),
                 description: String::from(
                     "Don't respond to this message. Use this tool when you would not like to respond.",
                 ),
@@ -60,26 +68,27 @@ impl Tool {
 
 #[cfg(test)]
 mod tests {
-    use super::Tool;
+    use super::ToolDefinition;
+    use super::literals;
     use serde_json::json;
 
     #[test]
     fn tool_serialization_with_properties() {
         let send_message =
-            serde_json::to_value(Tool::get_tools().first().unwrap().clone()).unwrap();
+            serde_json::to_value(ToolDefinition::get_tools().first().unwrap().clone()).unwrap();
 
         let json = json!({
-            "name": "send_message",
+            "name": literals::SEND_MESSAGE_NAME,
             "description": "Sends a message in the current Discord text channel. Use this tool when you want to send a message. The `message_content` defines the text that will be included in the message.",
             "input_schema": {
               "type": "object",
               "properties": {
-                "message_content": {
+                literals::SEND_MESSAGE_CONTENT_ARGUMENT_NAME: {
                   "type": "string",
                   "description": "The text to use for the Discord message body",
                 },
               },
-              "required": ["message_content"],
+              "required": [literals::SEND_MESSAGE_CONTENT_ARGUMENT_NAME],
             },
         });
 
@@ -89,10 +98,10 @@ mod tests {
     #[test]
     fn tool_serialization_with_no_properties() {
         let skip_response =
-            serde_json::to_value(Tool::get_tools().get(2).unwrap().clone()).unwrap();
+            serde_json::to_value(ToolDefinition::get_tools().get(2).unwrap().clone()).unwrap();
 
         let json = json!({
-            "name": "skip_response",
+            "name": literals::SKIP_RESPONSE_NAME,
             "description": "Don't respond to this message. Use this tool when you would not like to respond.",
             "input_schema": {
               "type": "object",
