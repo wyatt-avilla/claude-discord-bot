1d83c656b21d9a47ecd9d52d81debace1baa031c
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-08-04 17:54:39 -0700

    attachment support for message conversion

--- src/claude/message/message.rs
+++ src/claude/message/message.rs
@@ -1,4 +1,10 @@
-use crate::discord::NormalizeContent;
+use super::MediaType;
+use crate::{
+    claude::message::{ContentBlock, ImageBlock, TextBlock},
+    discord::NormalizeContent,
+};
+use base64::{Engine as _, engine::general_purpose};
+use futures::future::join_all;
 
 use poise::serenity_prelude as serenity;
 
@@ -12,7 +18,7 @@ pub struct Message {
 }
 
 impl Message {
-    pub fn from(discord_message: &serenity::Message, context: &serenity::Context) -> Self {
+    pub async fn from(discord_message: &serenity::Message, context: &serenity::Context) -> Self {
         let role = if discord_message.author.id == context.cache.current_user().id {
             Role::Assistant
         } else {
@@ -32,9 +38,47 @@ impl Message {
             discord_message.normalize_content(),
         );
 
-        Message {
-            role,
-            content: Content::Text(message_text),
+        let image_futures = discord_message.attachments.iter().map(|a| async move {
+            let media_type: MediaType = a.content_type.clone()?.as_str().try_into().ok()?;
+            let img_bytes = a.download().await.ok()?;
+            let b64 = general_purpose::STANDARD.encode(&img_bytes);
+
+            Some(ImageBlock {
+                media_type,
+                data: b64,
+            })
+        });
+
+        let imgs: Vec<ImageBlock> = join_all(image_futures)
+            .await
+            .into_iter()
+            .flatten()
+            .collect();
+
+        if imgs.is_empty() {
+            Message {
+                role,
+                content: Content::Text(message_text),
+            }
+        } else {
+            let cbs = imgs.into_iter().map(ContentBlock::ImageBlock);
+
+            if discord_message.content.is_empty() {
+                Message {
+                    role,
+                    content: Content::ContentBlocks(cbs.collect()),
+                }
+            } else {
+                Message {
+                    role,
+                    content: Content::ContentBlocks(
+                        cbs.chain(std::iter::once(ContentBlock::Text(TextBlock {
+                            text: message_text,
+                        })))
+                        .collect(),
+                    ),
+                }
+            }
         }
     }
 }
