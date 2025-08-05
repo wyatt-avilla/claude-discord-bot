e7ffe656288d456fbc97865f27f2ec85e649ee47
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-08-05 12:30:52 -0700

    get response for claude client

--- src/claude/client.rs
+++ src/claude/client.rs
@@ -1,6 +1,22 @@
 use super::model::Model;
+use super::response::Response;
 use super::system_prompt::SYSTEM_PROMPT;
+use super::tools::ToolDefinition;
+use futures::future::join_all;
 use std::num::NonZeroU64;
+use thiserror::Error;
+
+use poise::serenity_prelude as serenity;
+
+const MESSAGE_API_URL: &str = "https://api.anthropic.com/v1/messages";
+
+#[derive(Debug, Error)]
+pub enum ClaudeError {
+    #[error("Couldn't send HTTP request ({0})")]
+    Http(reqwest::Error),
+    #[error("Couldn't deserialize response ({0})")]
+    Parse(reqwest::Error),
+}
 
 pub struct Client {
     http: reqwest::Client,
@@ -8,6 +24,7 @@ pub struct Client {
     anthropic_version: String,
     model: Model,
     max_tokens: NonZeroU64,
+    tools: Vec<ToolDefinition>,
 }
 
 impl Client {
@@ -16,6 +33,39 @@ impl Client {
             model,
             ..Default::default()
         }
+    }
+
+    pub async fn get_response(
+        &self,
+        msgs: Vec<serenity::Message>,
+        ctx: &serenity::Context,
+        api_key: &str,
+    ) -> Result<Response, ClaudeError> {
+        let msgs = join_all(
+            msgs.into_iter()
+                .map(async move |m| super::Message::from(&m, ctx).await),
+        )
+        .await;
+
+        let request = super::Request::new(
+            self.model.clone(),
+            self.system_prompt.clone(),
+            self.max_tokens,
+            self.tools.clone(),
+            msgs,
+        );
+
+        self.http
+            .post(MESSAGE_API_URL)
+            .header("x-api-key", api_key)
+            .header("anthropic-version", self.anthropic_version.clone())
+            .json(&request)
+            .send()
+            .await
+            .map_err(ClaudeError::Http)?
+            .json()
+            .await
+            .map_err(ClaudeError::Parse)
     }
 }
 
@@ -27,6 +77,7 @@ impl Default for Client {
             model: Model::Sonnet4,
             max_tokens: NonZeroU64::new(2048).unwrap(),
             system_prompt: SYSTEM_PROMPT.to_string(),
+            tools: ToolDefinition::get_tools(),
         }
     }
 }
