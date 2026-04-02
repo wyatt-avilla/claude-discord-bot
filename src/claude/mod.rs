aa91dd8b2678083bdaa1070889b316afe840b91a
Wyatt Avilla <wyatt@linux.com>
2026-04-01 21:34:25 -0700

    clippy

--- src/claude/mod.rs
+++ src/claude/mod.rs
@@ -11,7 +11,11 @@ pub use client::{ClaudeError, Client, GetResponse};
 pub use conversation::Message;
 pub use model::Model;
 pub use request::Request;
-pub use response::{Action, Response, StopReason, Usage};
+pub use response::{Action, Response, StopReason};
+
+#[cfg(test)]
+pub use response::Usage;
+
 pub use tools::ToolDefinition;
 
 pub use system_prompt::MESSAGE_CONTEXT_LENGTH;
